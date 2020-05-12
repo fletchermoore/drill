fb3c8864fb88e527d8561f4874134a9dd6768864
Fletcher Moore <fletcherbmoore@gmail.com>
2020-05-12 19:52:35 -0400

    separate drill options and overview page

--- drill/__init__.py
+++ drill/__init__.py
@@ -5,19 +5,78 @@ from aqt.utils import showInfo
 # import all of the Qt GUI library
 from aqt.qt import *
 
-# We're going to add a menu item below. First we want to create a function to
-# be called when the menu item is activated.
+# imports added for my own code
+import copy
+from anki.decks import defaultDynamicDeck
+import drill.drilldeckconf as drillDiag
 
-def testFunction():
-    # get the number of cards in the current collection, which is stored in
-    # the main window
-    cardCount = mw.col.cardCount()
-    # show a message box
-    showInfo("Card count: %d" % cardCount)
 
-# create a new menu item, "test"
-action = QAction("test", mw)
-# set it to call testFunction when it's clicked
-action.triggered.connect(testFunction)
+# copy-paste of main.py onCram
+def onDrill(self, search=""):
+    n = 1
+    deck = mw.col.decks.current()
+    if not search:
+        if not deck["dyn"]:
+            search = 'deck:"%s" ' % deck["name"]
+    decks = mw.col.decks.allNames()
+    while _("Drill %d") % n in decks:
+        n += 1
+    name = _("Drill %d") % n
+    #did = mw.col.decks.newDyn(name)
+    did = newDrill(mw.col.decks, name) #replaces above line
+    diag = drillDiag.DeckConf(mw, first=True, search=search)
+    if not diag.ok:
+        # user cancelled first config
+        mw.col.decks.rem(did)
+        mw.col.decks.select(deck["id"])
+
+
+# copy-paste DeckManager.newDyn
+def newDrill(decks, name: str) -> int:
+    "Return a new dynamic deck and set it as the current deck."
+    deckDetails = copy.deepcopy(defaultDynamicDeck)
+    deckDetails['drill'] = True # adding our own flag so we can do something 
+                                # wild during review
+    did = decks.id(name, type=deckDetails)
+    decks.select(did)
+    return did
+
+
+# copy-paste w/ check for drill flag
+def patchedOverviewState(self, oldState: str) -> None:
+    if not self._selectedDeck():
+        return self.moveToState("deckBrowser")
+    self.col.reset()
+    if self.col.decks.current().get("drill", False):
+        if not hasattr(self, "drillOverview"):
+            from drill.drilloverview import DrillOverview
+            self.drillOverview = DrillOverview(self)
+        self.drillOverview.show()
+    else:
+        self.overview.show()
+
+# copy-paste w/ check for drill flag
+def patchedOnDeckConf(self, deck=None):
+    if not deck:
+        deck = self.col.decks.current()
+    if deck["dyn"]:
+        if not deck.get("drill", False):
+            import aqt.dyndeckconf
+            aqt.dyndeckconf.DeckConf(self, deck=deck)
+        else:
+            drillDiag.DeckConf(self, deck=deck)
+    else:
+        import aqt.deckconf
+
+        aqt.deckconf.DeckConf(self, deck)
+
+# Monkey Patching
+from aqt.main import AnkiQt
+AnkiQt._overviewState = patchedOverviewState
+AnkiQt.onDeckConf = patchedOnDeckConf
+
+# create a new menu item
+action = QAction("Drill", mw)
+action.triggered.connect(onDrill)
 # and add it to the tools menu
 mw.form.menuTools.addAction(action)
